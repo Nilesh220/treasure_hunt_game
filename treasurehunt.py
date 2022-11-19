cc309937fc6371f9a764ad1952a5f7446281f6d5
Nilesh220 <113936379+Nilesh220@users.noreply.github.com>
2022-11-19 23:34:45 +0530

    Update treasurehunt.py

--- treasurehunt.py
+++ treasurehunt.py
@@ -23,7 +23,6 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-#Write your code below this line 👇
 
 choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
 if choice1 == "left":
